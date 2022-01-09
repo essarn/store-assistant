7227f935988c2dc7eb198cc5de4a136da088f0e2
Isak <16906089+essarn@users.noreply.github.com>
2022-01-09 15:36:34 +0100

    chore: initial commit

---
+++ packages/server/app.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+const server = fastify()
+
+server.get('/ping', async (_request, _reply) => ({ message: 'pong' }))
+
+server.listen(8080, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Server listening at ${address}`)
+})
